9b6c575bf19b100eae7ba58cbf74bf338b14770a
RezoGraf <ctsaverno@gmail.com>
2021-09-04 00:01:33 +0700

    master

--- main.go
+++ main.go
@@ -7,12 +7,13 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-// type LOGIN struct {
-// 	USER     string `json:"user" binding:"required"`
-// 	PASSWORD string `json:"password" binding:"required"`
-// }
+type LOGIN struct {
+	USER     string `json:"user" binding:"required"`
+	PASSWORD string `json:"password" binding:"required"`
+}
 
 func main() {
 
@@ -35,6 +36,82 @@ func main() {
 		log.Printf("%q: %s\n", err, sqlStmt)
 		return
 	}
+	// tx, err := db.Begin()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// stmt, err := tx.Prepare("insert into foo(id, name) values(?, ?)")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer stmt.Close()
+	// for i := 0; i < 100; i++ {
+	// 	_, err = stmt.Exec(i, fmt.Sprintf("こんにちわ世界%03d", i))
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// }
+	// tx.Commit()
+
+	// rows, err := db.Query("select id, name from foo")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer rows.Close()
+	// for rows.Next() {
+	// 	var id int
+	// 	var name string
+	// 	err = rows.Scan(&id, &name)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	fmt.Println(id, name)
+	// }
+	// err = rows.Err()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// stmt, err = db.Prepare("select name from foo where id = ?")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer stmt.Close()
+	// var name string
+	// err = stmt.QueryRow("3").Scan(&name)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(name)
+
+	// _, err = db.Exec("delete from foo")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// _, err = db.Exec("insert into foo(id, name) values(1, 'foo'), (2, 'bar'), (3, 'baz')")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// rows, err = db.Query("select id, name from foo")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer rows.Close()
+	// for rows.Next() {
+	// 	var id int
+	// 	var name string
+	// 	err = rows.Scan(&id, &name)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	fmt.Println(id, name)
+	// }
+	// err = rows.Err()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
 	//--------------------gin-gonic---------------------------
 
