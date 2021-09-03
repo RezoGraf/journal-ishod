2197a014dbb7502557a9d219f333a4a58daf6892
RezoGraf <ctsaverno@gmail.com>
2021-09-03 11:22:52 +0700

    master

--- main.go
+++ main.go
@@ -1,17 +1,43 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
-type LOGIN struct {
-	USER     string `json:"user" binding:"required"`
-	PASSWORD string `json:"password" binding:"required"`
-}
+// type LOGIN struct {
+// 	USER     string `json:"user" binding:"required"`
+// 	PASSWORD string `json:"password" binding:"required"`
+// }
 
 func main() {
+
+	//-------------SQLITE--------------------------
+
+	os.Remove("./foo.db")
+
+	db, err := sql.Open("sqlite3", "./foo.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	sqlStmt := `
+	create table foo (id integer not null primary key, name text);
+	delete from foo;
+	`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return
+	}
+
+	//--------------------gin-gonic---------------------------
+
 	r := gin.Default()
 	r.GET("/status", func(c *gin.Context) {
 		c.String(200, "on")
